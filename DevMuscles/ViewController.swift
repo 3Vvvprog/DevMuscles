e152d573b31eb5d95499dd8422da1e69ea1bca5e
3Vvv <333vovk@mail.ru>
2024-02-10 17:48:10 +0300

    test font

--- DevMuscles/ViewController.swift
+++ DevMuscles/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    private var label = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = .white
+        
+        view.addSubview(label)
+        label.text = "Main Label"
+        label.textColor = .black
+        
+        
+        
+        
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
 
 
